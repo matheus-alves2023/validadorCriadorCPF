7b6683d509bf5954ba8baa0b0a363d83c9e067bb
matheus_alvesrs13@hotmail.com <matheusalvesrs20@gmail.com>
2023-07-23 22:10:45 -0300

    att

--- main.py
+++ main.py
@@ -28,14 +28,3 @@ def gerador_de_cpf_valido():
 
 
 
-
-# while True:
-
-#     option = input("Gostaria de GerarCPF ou validar um cpf? [1/2] ")
-
-#     if option == "1":
-
-#         gerador_de_cpf_valido()
-#     elif option == "2":
-
-#         validador_cpf()
